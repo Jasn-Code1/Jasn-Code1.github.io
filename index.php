65a2d4ab47383273d01fc814c80e4bcebccd02c7
Jasn-Code1 <139421008+Jasn-Code1@users.noreply.github.com>
2025-01-17 11:11:09 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    $conn = mysqli_connect(
+        "sql12.freesqldatabase.com",
+        "sql12758207",
+        "HkQSah6hhj",
+        "sql12758207"
+    );
+    if (isset($_POST["send"])) {
+        $message = $_POST["textcontent"];
+        if (strlen($message) > 0){
+            $addr = $_SERVER["REMOTE_ADDR"];
+            /* $output = fopen("output.txt", "a");
+            fwrite($output, "$addr:\n$message\n\n");
+            fclose($output); */
+            $sql = "INSERT INTO information (ip, message)
+                    VALUE ('$addr', '$message')";
+            mysqli_query($conn, $sql);
+            mysqli_close($conn);
+        }
+    }
+?>
+
+
+
+
+
