71707a1495535d74772362fa1306cb60d802a520
Jasn-Code1 <139421008+Jasn-Code1@users.noreply.github.com>
2025-01-17 14:58:08 -0500

    Add files via upload

---
+++ database.js
@@ -0,0 +1,23 @@
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+    host: 'sql12.freesqldatabase.com',
+    user: 'sql12758207',
+    password: 'HkQSah6hhj',
+    database: 'information'
+}).promise();
+
+const result = await pool.query("SELECT * FROM information");
+console.log(result);
+
+let textcontent = document.getElementById("textcontent");
+let send = document.getElementById("send");
+
+send.onclick = function() {
+    let msg = textcontent.value
+    if (msg.length > 0) {
+        pool.query(`INSERT INTO information (message) VALUE (${msg})`)
+        textcontent.value = "";
+    }
+}
+
